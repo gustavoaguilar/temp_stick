d2ac39e8f7b842e48ecc9c4c4428e4bace816574
gustavoaguilar <gustavooaguilar@gmail.com>
2024-01-14 20:54:37 -0300

    Created device class

---
+++ Firmware/TempStick/include/device.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#include<tuple>
+
+enum class ScreenState_t{
+    SCREEN_INTERNAL = 0,
+    SCREEN_EXTERNAL,
+    SCREEN_BLUETOOTH,
+    SCREEN_WIFI,
+};
+
+class Device{
+public:
+    bool IsInternalAvailable() const {return internal_.isAvailable;}
+    bool IsExternalAvailable() const {return external_.isAvailable;}
+
+    void InternalAvailable(bool status);
+    void ExternalAvailable(bool status);
+
+    void ButtonPress();
+    void UpdateInternalValues(std::tuple<float, float, float> new_values);
+    std::tuple<float, float, float> GetInternalValues();
+    
+    void UpdateExternalValues(float new_value);
+    float GetExternalValues();
+
+    ScreenState_t ScreenState(){ return screen_state_;}
+private:
+    struct{
+        bool isAvailable;
+        float temperature;
+        float humidity;
+        float pressure;
+    }internal_; // bme280
+    
+    struct{
+        bool isAvailable;
+        float temperature;
+    }external_; // onewire sensor... TODO: get the name of the sensor
+
+    ScreenState_t screen_state_ = ScreenState_t::SCREEN_INTERNAL;
+};
